e1ee1b6d43c2d77b2a0a83d5c561512cf75d9b54
Arshad Ansari <arshadansari27@gmail.com>
2017-02-18 14:26:38 +0530

    add rss

--- headlines.py
+++ headlines.py
@@ -2,14 +2,26 @@ from flask import Flask
 import feedparser
 
 app=Flask(__name__)
-BBC_FEED="http://feeds.bbci.co.uk/news/rss.xml"
+
+RSS_FEEDS={'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+	'cnn':'http://rss.ccn.com/rss/edition.rss',
+	'fox':'http: //feeds.foxnews.com/foxnews /latest',
+	'iol':'http://www.iol.co.za/cmlink/1.640'}
+
 @app.route("/")
-def get_news():
-	feed=feedparser.parse(BBC_FEED)
+@app.route("/bbc")
+def bbc():
+	return get_news('bbc')
+@app.route("/cnn")
+def cnn():
+	return get_news('cnn')
+
+def get_news(publication):
+	feed=feedparser.parse(RSS_FEEDS[publication])
 	first_article=feed['entries'][0]
 	return"""<html>
 	<body>
-	<h1>BBC HEADLINES</H1>
+	<h1> HEADLINES</H1>
 	<b>{0}</b> <br/>
 	<i>{1}</i> <br/>
 	<p>{2}</p> <br/>
