75adf340c9342a5e3ab1d0e8193089494a61d98a
komalunde <undekomal@gmail.com>
2017-02-20 19:02:31 +0530

    add html file

--- headlines.py
+++ headlines.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask import Flask
 import feedparser
 
@@ -16,16 +17,14 @@ def bbc():
 def cnn():
 	return get_news('cnn')
 
-def get_news(publication):
+@app.route("/")
+@app.route("/<publication>")
+def get_news(publication="bbc"):
 	feed=feedparser.parse(RSS_FEEDS[publication])
 	first_article=feed['entries'][0]
-	return"""<html>
-	<body>
-	<h1> HEADLINES</H1>
-	<b>{0}</b> <br/>
-	<i>{1}</i> <br/>
-	<p>{2}</p> <br/>
-      </body>
-</html>""".format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
+	return render_template("home.html",
+			title=first_article.get("title"),
+			published=first_article.get("published"),
+			summary=first_article.get("summary"))
 if __name__=='__main__':
    app.run(port=5000,debug=True)
