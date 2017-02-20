74db5a3f93da3dfce014b14f738ba44a14666d3b
komalunde <undekomal@gmail.com>
2017-02-20 19:58:24 +0530

    with jinja template

--- headlines.py
+++ headlines.py
@@ -22,9 +22,8 @@ def cnn():
 def get_news(publication="bbc"):
 	feed=feedparser.parse(RSS_FEEDS[publication])
 	first_article=feed['entries'][0]
-	return render_template("home.html",
-			title=first_article.get("title"),
-			published=first_article.get("published"),
-			summary=first_article.get("summary"))
+	return render_template("home.html",articles=feed['entries'])
+
+
 if __name__=='__main__':
-   app.run(port=5000,debug=True)
+   	app.run(port=5000,debug=True)
