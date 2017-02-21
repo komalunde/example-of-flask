3275eded9d8e0b5757a79880264f5303166d8079
komalunde <undekomal@gmail.com>
2017-02-21 16:21:47 +0530

    add weather data

--- headlines.py
+++ headlines.py
@@ -1,4 +1,6 @@
-
+import json
+import urllib2
+import urllib
 import feedparser
 from flask import Flask
 from flask import render_template
@@ -20,8 +22,31 @@ def bbc():
 def cnn():
 	return get_news('cnn')
 
+DEFACULTS={'publication':'bbc',
+		   'city':'London,UK'}
+
+@app.route("/")
+def home():
+	publication=request.args.get('publication')
+	if not publication:
+		publication=DEFACULTS['publication']
+		articles=get_news(publication)
+		city=request.args.get('city')
+		if not city:
+			city=DEFACULTS['city']
+			weather=get_weather(city)
+		return render_template("home.html",
+				articles=articles,weather=weather)
+
 
 @app.route("/<publication>")
+def get_news(query):
+	if not query or query.lower() not in RSS_FEEDS:
+		publication=DEFACULTS["publication"]
+	else:
+		publication=query.lower()
+		feed=feedparser.parse(RSS_FEEDS[publication])
+		return feed['entries']
 
 def get_news(publication):
 	query = request.args.get("publication")
@@ -29,9 +54,26 @@ def get_news(publication):
 			publication="bbc"
 	else:
 		publication = query.lower()
-	feed=feedparser.parse(RSS_FEEDS[publication])
-	return render_template("home.html",
-	articles=feed['entries'])
+	feed = feedparser.parse(RSS_FEEDS[publication])
+	weather = get_weather("london")
+	return render_template("home.html", articles=feed["entries"], weather=weather)
+
+
+def get_weather(query):
+	api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=cb3823bdd1813a699aa834b14a69cb99'
+
+	query = urllib.quote(query)
+	url = api_url.format(query)
+	data = urllib2.urlopen(url).read()
+	parsed = json.loads(data)
+	weather=None
+	if parsed.get("weather"):
+		weather={"description":
+				 parsed["weather"][0]["description"],
+				 "temperature":parsed["main"]["temp"],
+				 "city":parsed["name"]
+				 }
+		return weather
 
 
 if __name__=='__main__':
